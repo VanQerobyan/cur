b33144bea266e22e69ba0ce2ac01413704d097fb
VanQerobyan <qerobyanvan23@gmail.com>
2026-02-11 17:13:31 +0400

    Add localization strings for Armenian and English

---
+++ Strings.kt
@@ -0,0 +1,12 @@
+package com.yourpackage
+
+val strings = mapOf(
+    "en" to mapOf(
+        "welcome" to "Welcome!",
+        "goodbye" to "Goodbye!"
+    ),
+    "hy" to mapOf(
+        "welcome" to "Բարի գալուստ!",
+        "goodbye" to "Հանգիստ!"
+    )
+)
